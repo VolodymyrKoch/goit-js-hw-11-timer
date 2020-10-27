ad33d247209fba7a68b6b821079840edc993c5ca
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-10-27 22:48:14 +0200

    g

---
+++ js/task3.js
@@ -0,0 +1,38 @@
+const dayRef = document.querySelector('[data-value= "days"]');
+const hoursRef = document.querySelector('[data-value="hours"]');
+const minsRef = document.querySelector('[data-value="mins"]');
+const secsRef = document.querySelector('[data-value="secs"]');
+const timer = document.querySelector("#timer-1");
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  timer = setInterval(() => {
+    const time = this.targetDate - Date.now();
+    this.updateClockface(time);
+  }, 1000);
+
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    dayRef.textContent = days;
+    hoursRef.textContent = hours;
+    minsRef.textContent = mins;
+    secsRef.textContent = secs;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jan 01, 2021"),
+});
+
+
