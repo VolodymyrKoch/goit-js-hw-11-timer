b1788b0aaf204f642b4b6b66579d188493ceb69b
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-10-29 18:12:51 +0200

    виправив на два лічильника

--- js/task3.js
+++ js/task3.js
@@ -1,13 +1,9 @@
-const dayRef = document.querySelector('[data-value= "days"]');
-const hoursRef = document.querySelector('[data-value="hours"]');
-const minsRef = document.querySelector('[data-value="mins"]');
-const secsRef = document.querySelector('[data-value="secs"]');
-const timer = document.querySelector("#timer-1");
 
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.selector = selector;
+    this.selector = document.querySelector(selector);
     this.targetDate = targetDate;
+    
   }
 
   timer = setInterval(() => {
@@ -22,13 +18,14 @@ class CountdownTimer {
     const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-    dayRef.textContent = days;
-    hoursRef.textContent = hours;
-    minsRef.textContent = mins;
-    secsRef.textContent = secs;
+    
+    this.selector.querySelector('[data-value="days"]').textContent=days;
+    this.selector.querySelector('[data-value="hours"]').textContent=hours;
+    this.selector.querySelector('[data-value="mins"]').textContent=mins;
+    this.selector.querySelector('[data-value="secs"]').textContent=secs;
   }
-
-  pad(value) {
+ 
+   pad(value) {
     return String(value).padStart(2, "0");
   }
 }
@@ -37,4 +34,8 @@ new CountdownTimer({
   targetDate: new Date("Jan 01, 2021"),
 });
 
+new CountdownTimer({
+  selector: "#timer-2",
+  targetDate: new Date("Dec 16, 2020"),
+});
 
